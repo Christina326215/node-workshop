38f472d0619c64fefbc4065f7dee564f94f88b59
Christina326215 <christina326215@gmail.com>
2021-08-13 00:53:40 +0800

    add trim

--- crawler/crawler3_await.js
+++ crawler/crawler3_await.js
@@ -4,19 +4,24 @@ const axios = require("axios");
 const moment = require("moment");
 const fs = require("fs");
 
-// let stockCode = "";
+// async 非同步，是暫停的限制範圍。
 async function crawlStock() {
   try {
+    // await 暫停鍵
     let stockCode = await new Promise((resolve, reject) => {
       fs.readFile("stock.txt", "utf-8", (err, stockCode) => {
         if (err) {
           reject(err);
         } else {
+          // https://developer.mozilla.org/zh-TW/docs/Web/JavaScript/Reference/Global_Objects/String/Trim
+          // trim 移除前後的空白字元，包括換行
           resolve(stockCode.trim());
-        //   resolve(stockCode);
+          //   resolve(stockCode);
         }
       });
     });
+
+    // await 暫停鍵
     let response = await axios.get(
       "https://www.twse.com.tw/exchangeReport/STOCK_DAY",
       {
@@ -31,6 +36,11 @@ async function crawlStock() {
     console.log(response.data.title);
   } catch (err) {
     console.log(err);
+    // console.error(e);
+    // console.warn();
+    // console.info();
+    // console.trace();
+    // console.debug();
   } finally {
     console.log("got the data");
   }
