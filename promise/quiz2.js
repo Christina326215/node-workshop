a1ce09d30d0bbe5ba25d1617e81309446a7800f7
Christina326215 <christina326215@gmail.com>
2021-08-12 23:53:04 +0800

    async await order

--- promise/quiz2.js
+++ promise/quiz2.js
@@ -1,19 +1,24 @@
 async function asyncF() {
-    console.log(1);
-    await new Promise((resolve, reject) => {
-      setTimeout(() => {
-        console.log(2);
-        resolve();
-      }, 0);
-    });
-    console.log(3);
-  }
-  
-  console.log(4);
-  asyncF();
-  console.log(5);
+  console.log(1);
+  // await 暫停鍵
+  await new Promise((resolve, reject) => {
+    console.log("A");
+    // 非同步 --> single-thread 還是要去把工作交接出去
+    setTimeout(() => {
+      console.log(2);
+      resolve();
+      // reject
+    }, 0);
+  });
+  console.log(3);
+}
+console.log(4);
+asyncF();
+console.log(5);
 
-// 結果：4 1 5 2 3
+// 結果：
+// 有await: 4 -> 1 -> A -> 5 -> 2 -> 3
+// 無await: 4 -> 1 -> A -> 3 -> 5 -> 2
 // 1. 程式由第12行開始，console.log(4)，所以第一個結果為4。
 // 2. 接著回到第一行，呼叫function asyncF()，非同步async、把工作丟給暗樁做。
 // 3. 執行function syncF()，並且console.log(1)，所以第二個結果為1。
