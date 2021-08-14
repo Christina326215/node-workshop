e8fc0da74ab94fb24d3be795ff5e742d0028ddfa
Christina326215 <christina326215@gmail.com>
2021-08-14 23:57:39 +0800

    update

--- module/do.js
+++ module/do.js
@@ -1,4 +1,16 @@
 let phone = require("./phone");
 
-// console.log(phone);
-console.log(phone.setBrand());
+phone.setOwner("Tina");
+console.log(phone);  
+console.log(phone.showBrand());  
+console.log(phone.showOwner());   
+
+// 結果：
+// {
+//     showBrand: [Function: showBrand],
+//     showColor: [Function: showColor],
+//     showOwner: [Function: showOwner],
+//     setOwner: [Function: setOwner]
+//   }
+//   iphone
+//   Tina
