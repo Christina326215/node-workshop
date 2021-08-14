6768e2d97641c4c7a17229d1a4210ddc1c95ae60
Christina326215 <christina326215@gmail.com>
2021-08-14 23:57:19 +0800

    update

--- module/phone.js
+++ module/phone.js
@@ -1,43 +1,57 @@
 // 模組範例
 
+// 在建立新模組之前，建立一個新的空物件。
+// exports = module.exports = {};
+// 也就是module.exports = {};
+// 然後exports = module.exports;
+// 兩者都指向同一個物件。
+
+
 let brand = "iphone";
 let color = "white";
-let owner = "Christina";
+let owner = "";
 
-function setBrand() {
+function showBrand() {
   return brand;
 }
 
-function setColor() {
+function showColor() {
   return color;
 }
 
-function setOwner() {
+function showOwner() {
   return owner;
 }
 
+function setOwner(name) {
+  owner = name;
+}
+
+// exports = module.exports = {};
+
+// exports.showBrand = showBrand;
+// exports.showColor = showColor;
+// exports.showOwner = showOwner;
+// 使用exports(而非底層預設的module.exports)輸出，並給定新的空物件內容。
+
+exports = {
+ showBrand: showBrand,
+ showColor: showColor,
+ showOwner: showOwner,
+ setOwner: setOwner,
+};
+// 以exports的方式輸出module，其實是在預設好的return module.exports以外又新建一個物件，但事實上無法傳回exports結果，而是return module.export空的結果，因此發生錯誤。
+
+// module.exports.showBrand = showBrand;
+
 module.exports = {
-  setBrand,
-  setColor,
+  showBrand,
+  showColor,
+  showOwner,
   setOwner,
 };
+// 也是建立新物件
+
 
 // return module.exports
 // 以上重點：無論如何底層都會設定以module.exports的方式return輸出。
-
-
-// ----------------------------------
-// 以下以exports的方式return輸出來舉例：
-// exports = module.exports = {};
-
-// exports.setBrand = setBrand;
-// exports.setColor = setColor;
-// exports.setOwner = setOwner;
-// 使用exports(而非底層預設的module.exports)輸出，並給定新的空物件內容。
-
-// exports = {
-//   setBrand,
-//   setColor,
-//   setOwner,
-// };
-// 以exports的方式輸出module，其實是在預設好的return module.exports以外又新建一個物件，但事實上無法傳回exports結果。
