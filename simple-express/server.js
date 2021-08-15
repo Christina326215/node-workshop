9400e54ad5bf09183ab175506d75d2fc03f5e48e
Christina326215 <christina326215@gmail.com>
2021-08-15 11:29:50 +0800

    add app.use

--- simple-express/server.js
+++ simple-express/server.js
@@ -3,9 +3,20 @@ const express = require("express");
 // 利用express 建立了一個express application
 let app = express();
 
+app.use((request,response,next) => {
+    let current = new Date();
+    console.log(`Hey, 有人來訪問囉 at ${current.toISOString()}`);
+    next();
+})
+
+app.use((request,response,next) => {
+    console.log("Haha, 我是第二個中間件");
+    next();
+})
+
 // http method: get, post, put, patch, delete......
 app.get("/",function(request, response,next){
-    response.send("hello, this is my first express.");
+    response.send("hello, this is my first express, with nodemon.");
 });
 
 app.get("/about",function(request, response,next){
